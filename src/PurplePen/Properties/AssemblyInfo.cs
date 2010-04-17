a82967f614b1bb3e3481f6c7c3eff18e166def99
Peter Golde <peter@golde.org>
2010-04-17 15:08:19 -0700

    Combine both map models into single solution; removed non-map Purple stuff

--- src/PurplePen/Properties/AssemblyInfo.cs
+++ src/PurplePen/Properties/AssemblyInfo.cs
@@ -22,6 +22,11 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f4eead18-8c4a-46b1-be28-9c6eb7222e8f")]
 
+[assembly: AssemblyVersion(VersionNumber.Current)]
+[assembly: AssemblyFileVersion(VersionNumber.Current)]
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("PurplePen_Tests")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -29,7 +34,16 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion(PurplePen.VersionNumber.Current)]
-[assembly: AssemblyFileVersion(PurplePen.VersionNumber.Current)]
 
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("PurplePen_Tests")]
+static class VersionNumber
+{
+    public const string Current = "1.0.1.5000";
+
+    // The last component encodes the Alpha/Beta/RC/Stable notion.
+    // 1100 is Alpha 1, 2200 is Beta 2, 5000 is stable release
+    public const int Alpha = 1000;
+    public const int Beta = 2000;
+    public const int RC = 3000;
+    public const int Stable = 5000;
+}
+
