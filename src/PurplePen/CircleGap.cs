ef1e12dd5ae8ac6f08ddf2150f5889d41429dad0
Peter Golde <peter@golde.org>
2013-02-04 15:35:25 -0800

    Forgot to add files.

---
+++ src/PurplePen/CircleGap.cs
@@ -0,0 +1,259 @@
+﻿/* Copyright (c) 2013, Peter Golde
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are 
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of Peter Golde, nor "Purple Pen", nor the names
+ * of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Diagnostics;
+
+using PurplePen.MapModel;
+using PurplePen.Graphics2D;
+
+namespace PurplePen
+{
+    // A gap in a circle. Angle of start and stop, in degrees. Angles are counter-clockwise from X-axis in
+    // cartesian (positive Y is up) coordinate system.
+    public struct CircleGap
+    {
+        public readonly float startAngle;            // Start angle of gap, in degrees
+        public readonly float stopAngle;             // stop angle of gap
+
+        public CircleGap(float startAngle, float stopAngle)
+        {
+            this.startAngle = startAngle;
+            this.stopAngle = stopAngle;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is CircleGap))
+                return false;
+
+            return (this == ((CircleGap)obj));
+        }
+
+        public override int GetHashCode()
+        {
+            return startAngle.GetHashCode() ^ stopAngle.GetHashCode();
+        }
+
+        public static bool operator ==(CircleGap gap1, CircleGap gap2)
+        {
+            return (gap1.startAngle == gap2.startAngle && gap1.stopAngle == gap2.stopAngle);
+        }
+
+        public static bool operator !=(CircleGap gap1, CircleGap gap2)
+        {
+            return !(gap1 == gap2);
+        }
+
+        /*
+         * Static functions that work on arrays of circle gaps.
+         */
+
+        // Get the start/stop points of the gaps. Primarily useful for finding where the handles should be.
+        public static PointF[] GapStartStopPoints(PointF center, float radius, CircleGap[] gaps)
+        {
+            if (gaps == null || gaps.Length == 0)
+                return null;
+
+            PointF[] pts = new PointF[gaps.Length * 2];
+            for (int i = 0; i < gaps.Length; ++i) {
+                pts[i * 2] = Geometry.MoveDistance(center, radius, gaps[i].startAngle);
+                pts[i * 2 + 1] = Geometry.MoveDistance(center, radius, gaps[i].stopAngle);
+            }
+
+            return pts;
+        }
+
+        // Get a series of arc start angle/sweep angle pairs to draw a circle with the gaps. Gaps must be in simplified form.
+        public static float[] ArcStartSweeps(CircleGap[] gaps)
+        {
+            if (gaps == null || gaps.Length == 0)
+                return new float[2] { 0F, 360F };
+
+            float[] arcs = new float[gaps.Length * 2];
+            for (int i = 0; i < gaps.Length; i += 1) {
+                float startArc = gaps[i].stopAngle;
+                float endArc = (i == gaps.Length - 1) ? gaps[0].startAngle : gaps[i + 1].startAngle;
+                arcs[i * 2] = startArc;
+                arcs[i * 2 + 1] = (endArc - startArc + 360.0F) % 360.0F;
+            }
+            return arcs;
+        }
+
+#if false
+        // Move a gap start/stop point to a new location. Return the new gap array. The gap array is NOT simplified.
+        public static LegGap[] MoveStartStopPoint(SymPath path, LegGap[] gaps, PointF oldPt, PointF newPt)
+        {
+            LegGap[] newGaps = (LegGap[])gaps.Clone();
+            float newLengthAlongPath = path.LengthToPoint(newPt);
+
+            for (int i = 0; i < newGaps.Length; ++i) {
+                PointF startPt = path.PointAtLength(gaps[i].distanceFromStart);
+                PointF endPt = path.PointAtLength(gaps[i].distanceFromStart + gaps[i].length);
+
+                if (Geometry.Distance(startPt, oldPt) < 0.01) {
+                    // Moving start point of the gap.
+                    newGaps[i].length -= (newLengthAlongPath - newGaps[i].distanceFromStart);
+                    newGaps[i].distanceFromStart = newLengthAlongPath;
+                }
+                else if (Geometry.Distance(endPt, oldPt) < 0.01) {
+                    // Moving end point of the gap.
+                    newGaps[i].length = newLengthAlongPath - gaps[i].distanceFromStart;
+                }
+            }
+
+            return newGaps;
+        }
+#endif
+
+        // Add a new gap an an array of gaps. The resulting array is simplified. The original array may be null.
+        public static CircleGap[] AddGap(CircleGap[] original, float startAngle, float stopAngle)
+        {
+            CircleGap newGap;
+            CircleGap[] newGaps;
+
+            // Create the new gap.
+            newGap = new CircleGap(startAngle, stopAngle);
+
+            // Add to the old gaps.
+            if (original == null)
+                newGaps = new CircleGap[1] { newGap };
+            else {
+                newGaps = new CircleGap[original.Length + 1];
+                Array.Copy(original, newGaps, original.Length);
+                newGaps[original.Length] = newGap;
+            }
+
+            // Simplify
+            return SimplifyGaps(newGaps);
+        }
+
+        // Simplify a gap array. Sorts in order, combines overlapping gaps, and removes gaps before and beyond end of length.
+        public static CircleGap[] SimplifyGaps(CircleGap[] gaps)
+        {
+            if (gaps == null || gaps.Length == 0)
+                return null;
+
+            List<CircleGap> newGaps = new List<CircleGap>();
+
+            // Remove gaps that have start before end.
+            // Simplify to end angle between 0 and 360, startAngle between -360 and end angle.
+            int i;
+            for (i = 0; i < gaps.Length; ++i) {
+                if (gaps[i].stopAngle <= gaps[i].startAngle)
+                    continue;
+
+                float stop = gaps[i].stopAngle % 360.0F;
+                if (stop < 0)
+                    stop += 360.0F;
+                float start = gaps[i].startAngle % 360.0F;
+                if (start < 0)
+                    start += 360.0F;
+                if (start > stop)
+                    start -= 360.0F;
+
+                newGaps.Add(new CircleGap(start, stop));
+            }
+
+            // Sort gaps by their start angle.
+            newGaps.Sort(delegate(CircleGap gap1, CircleGap gap2) {
+                if (gap1.startAngle < gap2.startAngle)
+                    return -1;
+                else if (gap1.startAngle > gap2.startAngle)
+                    return 1;
+                else
+                    return 0;
+            });
+
+            // Combine gaps that overlap.
+            i = 0;
+            while (i < newGaps.Count - 1) {
+                if (newGaps[i].stopAngle >= newGaps[i + 1].startAngle) {
+                    if (newGaps[i].stopAngle  < newGaps[i + 1].stopAngle)
+                        newGaps[i] = new CircleGap(newGaps[i].startAngle, newGaps[i + 1].stopAngle);
+                    newGaps.RemoveAt(i + 1);
+                }
+                else
+                    ++i;
+            }
+
+            // Last and first can overlap
+            while (newGaps.Count >= 2 && newGaps[0].startAngle < newGaps[newGaps.Count - 1].stopAngle - 360.0F) {
+                newGaps[0] = new CircleGap(newGaps[newGaps.Count - 1].startAngle - 360.0F, newGaps[0].stopAngle);
+                newGaps.RemoveAt(newGaps.Count - 1);
+            }
+
+            // And we're done.
+            return (newGaps.Count > 0) ? newGaps.ToArray() : null;
+        }
+
+        public static CircleGap[] ComputeCircleGaps(uint gaps)
+        {
+            if (gaps == 0xFFFFFFFF)
+                return null;                       // no gaps
+            else if (gaps == 0)
+                return new CircleGap[1] { new CircleGap(-360F, 0) };  // all gap
+            else {
+                int firstGap = 0;
+
+                // Find the first gap start (a 1 to 0 transition).
+                for (int i = 0; i < 32; ++i) {
+                    if (!Util.GetBit(gaps, i) && Util.GetBit(gaps, i - 1)) {
+                        firstGap = i;
+                        break;
+                    }
+                }
+
+                List<CircleGap> gapList = new List<CircleGap>();
+                // Now create gaps.
+                int lastGapStart = firstGap;
+                for (int i = firstGap; i < firstGap + 32; ++i) {
+                    if (Util.GetBit(gaps, i) && !Util.GetBit(gaps, i - 1)) {
+                        // found end of gap.
+                        int endGap = i;
+
+                        gapList.Add(new CircleGap((float)(lastGapStart * 360.0 / 32), (float)(endGap * 360.0 / 32)));
+                    }
+                    else if (!Util.GetBit(gaps, i) && Util.GetBit(gaps, i - 1)) {
+                        lastGapStart = i;
+                    }
+                }
+
+                return SimplifyGaps(gapList.ToArray());
+            }
+        }
+    }
+}
---
+++ src/PurplePen_Tests/PurplePen/CircleGapTests.cs
@@ -0,0 +1,226 @@
+﻿/* Copyright (c) 2013, Peter Golde
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are 
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of Peter Golde, nor "Purple Pen", nor the names
+ * of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ */
+
+#if TEST
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+using PurplePen.MapModel;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestingUtils;
+
+namespace PurplePen.Tests
+{
+
+    [TestClass]
+    public class CircleGapTests
+    {
+        [TestMethod]
+        public void SimplifyGaps()
+        {
+            CircleGap[] gaps = new CircleGap[] { new CircleGap(125, 134), new CircleGap(290, 270), new CircleGap(125, 140), 
+                new CircleGap(-40, -20), new CircleGap(180, 180), new CircleGap(-30, 45), new CircleGap(50, 100), 
+                new CircleGap(-140, -120), new CircleGap(55, 65), new CircleGap(75, 102), new CircleGap(129, 130) };
+            CircleGap[] expected = new CircleGap[] { new CircleGap(-40, 45), new CircleGap(50, 102), new CircleGap(125, 140), new CircleGap(220, 240) };
+
+            CircleGap[] result = CircleGap.SimplifyGaps(gaps);
+            Assert.AreEqual(expected.Length, result.Length);
+            for (int i = 0; i < result.Length; ++i)
+                Assert.IsTrue(expected[i] == result[i]);
+        }
+
+        [TestMethod]
+        public void GapStartStopPoints()
+        {
+            CircleGap[] gaps = new CircleGap[] { new CircleGap(-45, 30), new CircleGap(60, 270), new CircleGap(290, 300) };
+            PointF[] result = CircleGap.GapStartStopPoints(new PointF(7F, 13F), 10F, gaps);
+            PointF[] expected = new PointF[] { new PointF((float) (10.0 * Math.Sqrt(2)/2 + 7), (float) (10.0 * -Math.Sqrt(2)/2 + 13)),
+                                               new PointF((float) (10.0 * Math.Sqrt(3)/2 + 7), (float) (5 + 13)),
+                                               new PointF((float) (5 + 7), (float) (10.0 * Math.Sqrt(3)/2 + 13)),
+                                               new PointF((float) ( 7), (float) (-10 + 13)),
+                                               new PointF((float) (10.0 * Math.Cos(2 * Math.PI * 290 / 360) + 7), (float) (10.0 * Math.Sin(2 * Math.PI * 290 / 360) + 13)),
+                                               new PointF((float) (10.0 * Math.Cos(2 * Math.PI * 300 / 360) + 7), (float) (10.0 * Math.Sin(2 * Math.PI * 300 / 360) + 13)),
+            };
+            TestUtil.TestEnumerableAnyOrder(result, expected);
+
+            result = CircleGap.GapStartStopPoints(new PointF(7F, 13F), 10F, null);
+            Assert.IsNull(result);
+
+            result = CircleGap.GapStartStopPoints(new PointF(7F, 13F), 10F, new CircleGap[0]);
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void ArcStartSweeps()
+        {
+            CircleGap[] gaps = new CircleGap[] { new CircleGap(-45, 30), new CircleGap(55, 270), new CircleGap(290, 300) };
+            float[] result = CircleGap.ArcStartSweeps(gaps);
+            float[] expected = new float[] { 30F, 25F, 270F, 20F, 300F, 15F};
+            
+            Assert.AreEqual(expected.Length, result.Length);
+
+            for (int i = 0; i < expected.Length; ++i) {
+                Assert.AreEqual(expected[i], result[i]);
+            }
+
+            result = CircleGap.ArcStartSweeps(null);
+            Assert.AreEqual(0F, result[0]);
+            Assert.AreEqual(360F, result[1]);
+        }
+
+        [TestMethod]
+        public void ComputeCircleGaps()
+        {
+            CircleGap[] result;
+
+            result = CircleGap.ComputeCircleGaps(0xFFFFFFFF);
+            Assert.IsNull(result);
+
+            result = CircleGap.ComputeCircleGaps(0);
+            Assert.AreEqual(1, result.Length);
+            Assert.AreEqual(new CircleGap(-360, 0), result[0]);
+
+            result = CircleGap.ComputeCircleGaps(0x03FF0060);
+            Assert.AreEqual(2, result.Length);
+            Assert.AreEqual(new CircleGap(-67.5F, 56.25F), result[0]);
+            Assert.AreEqual(new CircleGap(78.75F, 180.0F), result[1]);
+
+            result = CircleGap.ComputeCircleGaps(0xF3F1006F);
+            Assert.AreEqual(4, result.Length);
+            Assert.AreEqual(new CircleGap(45F, 56.25F), result[0]);
+            Assert.AreEqual(new CircleGap(78.75F, 180F), result[1]);
+            Assert.AreEqual(new CircleGap(191.25F, 225F), result[2]);
+            Assert.AreEqual(new CircleGap(292.5F, 315.0F), result[3]);
+        
+        }
+
+#if false
+        [TestMethod]
+        public void MoveGapsToNewPath()
+        {
+            SymPath oldPath = new SymPath(new PointF[] { new PointF(0, 0), new PointF(10, 10) });
+            CircleGap[] oldGaps = new CircleGap[] { new CircleGap(1, 2), new CircleGap(7, 0.5F), new CircleGap(12, 2) };
+            SymPath newPath = new SymPath(new PointF[] { new PointF(10, 10), new PointF(10, 0), new PointF(0, 0) });
+            CircleGap[] expected = new CircleGap[] { new CircleGap(0.1F, 1.414F), new CircleGap(17.88F, 1.414F) };
+
+            CircleGap[] newGaps = CircleGap.MoveGapsToNewPath(oldGaps, oldPath, newPath);
+
+            // Make sure result and expected match.
+            Assert.AreEqual(expected.Length, newGaps.Length);
+            for (int i = 0; i < newGaps.Length; ++i) {
+                Assert.AreEqual(expected[i].distanceFromStart, newGaps[i].distanceFromStart, 0.01F);
+                Assert.AreEqual(expected[i].length, newGaps[i].length, 0.01F);
+            }
+        }
+
+        [TestMethod]
+        public void GapStartStopPoints()
+        {
+            SymPath path = new SymPath(new PointF[] { new PointF(10, 10), new PointF(10, 0), new PointF(0, 0) });
+            CircleGap[] gaps = new CircleGap[] { new CircleGap(1, 2), new CircleGap(7, 0.5F), new CircleGap(12, 2) };
+            PointF[] expected = new PointF[] { new PointF(10, 9), new PointF(10, 7), new PointF(10, 3), new PointF(10, 2.5F), new PointF(8, 0), new PointF(6, 0) };
+
+            PointF[] result = CircleGap.GapStartStopPoints(path, gaps);
+            TestUtil.TestEnumerableAnyOrder(result, expected);
+        }
+
+        [TestMethod]
+        public void MoveStartStopPoint()
+        {
+            SymPath path = new SymPath(new PointF[] { new PointF(10, 10), new PointF(10, 0), new PointF(0, 0) });
+            CircleGap[] gaps = new CircleGap[] { new CircleGap(1, 2), new CircleGap(7, 0.5F), new CircleGap(12, 2) };
+
+            CircleGap[] result = CircleGap.MoveStartStopPoint(path, gaps, new PointF(8, 0), new PointF(8.5F, 0));
+            CircleGap[] expected = new CircleGap[] { new CircleGap(1, 2), new CircleGap(7, 0.5F), new CircleGap(11.5F, 2.5F) };
+            TestUtil.TestEnumerableAnyOrder(result, expected);
+
+            result = CircleGap.MoveStartStopPoint(path, gaps, new PointF(10, 2.5F), new PointF(8, 2));
+            expected = new CircleGap[] { new CircleGap(1, 2), new CircleGap(7, 1F), new CircleGap(12, 2) };
+            TestUtil.TestEnumerableAnyOrder(result, expected);
+        }
+
+        [TestMethod]
+        public void AddGap1()
+        {
+            // Add a gap to a null gap array.
+            SymPath path = new SymPath(new PointF[] { new PointF(10, 10), new PointF(10, 0), new PointF(0, 0) });
+            PointF pt1 = new PointF(7, -2);
+            PointF pt2 = new PointF(11, 4);
+
+            CircleGap[] gaps = CircleGap.AddGap(path, null, pt1, pt2);
+            Assert.AreEqual(1, gaps.Length);
+            Assert.AreEqual(6, gaps[0].distanceFromStart);
+            Assert.AreEqual(7, gaps[0].length);
+
+        }
+
+        [TestMethod]
+        public void AddGap2()
+        {
+            // Add a gap to an existing array.
+            SymPath path = new SymPath(new PointF[] { new PointF(10, 10), new PointF(10, 0), new PointF(0, 0) });
+            CircleGap[] oldGaps = { new CircleGap(6, 7) };
+            PointF pt1 = new PointF(8, -2);
+            PointF pt2 = new PointF(11, 8.5F);
+
+            CircleGap[] gaps = CircleGap.AddGap(path, oldGaps, pt1, pt2);
+            Assert.AreEqual(1, gaps.Length);
+            Assert.AreEqual(1.5F, gaps[0].distanceFromStart);
+            Assert.AreEqual(11.5F, gaps[0].length);
+
+            pt1 = new PointF(3, -2);
+            pt2 = new PointF(1.5F, 1);
+            gaps = CircleGap.AddGap(path, oldGaps, pt1, pt2);
+            Assert.AreEqual(2, gaps.Length);
+            Assert.AreEqual(6, gaps[0].distanceFromStart);
+            Assert.AreEqual(7, gaps[0].length);
+            Assert.AreEqual(17F, gaps[1].distanceFromStart);
+            Assert.AreEqual(1.5F, gaps[1].length);
+        }
+
+#endif
+
+
+    }
+}
+
+#endif //TEST
