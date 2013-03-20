38e635c1834327d695c23df5823a7145e388bbed
Peter Golde <peter@golde.org>
2013-03-20 13:54:44 -0700

    More minimalist downloading dialog.

--- src/PurplePen/DownloadProgressDialog.Designer.cs
+++ src/PurplePen/DownloadProgressDialog.Designer.cs
@@ -27,51 +27,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.labelDownloading = new System.Windows.Forms.Label();
             this.progressBar = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // okButton
             // 
-            this.okButton.Location = new System.Drawing.Point(204, 121);
+            this.okButton.Location = new System.Drawing.Point(177, 20);
             this.okButton.Text = "";
             this.okButton.Visible = false;
             // 
             // cancelButton
             // 
-            this.cancelButton.Location = new System.Drawing.Point(300, 121);
-            // 
-            // labelDownloading
-            // 
-            this.labelDownloading.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.labelDownloading.Location = new System.Drawing.Point(13, 13);
-            this.labelDownloading.Name = "labelDownloading";
-            this.labelDownloading.Size = new System.Drawing.Size(374, 39);
-            this.labelDownloading.TabIndex = 6;
-            this.labelDownloading.Text = "Downloading new version...";
+            this.cancelButton.Location = new System.Drawing.Point(300, 16);
             // 
             // progressBar
             // 
             this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.progressBar.Location = new System.Drawing.Point(16, 67);
+            this.progressBar.Location = new System.Drawing.Point(12, 16);
             this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(371, 23);
+            this.progressBar.Size = new System.Drawing.Size(282, 23);
             this.progressBar.TabIndex = 7;
             // 
             // DownloadProgressDialog
             // 
             this.AcceptButton = null;
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
-            this.ClientSize = new System.Drawing.Size(399, 155);
+            this.ClientSize = new System.Drawing.Size(399, 54);
+            this.ControlBox = false;
             this.Controls.Add(this.progressBar);
-            this.Controls.Add(this.labelDownloading);
+            this.HelpButton = false;
             this.Name = "DownloadProgressDialog";
-            this.Text = "Downloading";
+            this.Text = "Downloading Purple Pen";
             this.Controls.SetChildIndex(this.okButton, 0);
             this.Controls.SetChildIndex(this.cancelButton, 0);
-            this.Controls.SetChildIndex(this.labelDownloading, 0);
             this.Controls.SetChildIndex(this.progressBar, 0);
             this.ResumeLayout(false);
 
@@ -79,7 +68,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Label labelDownloading;
         private System.Windows.Forms.ProgressBar progressBar;
     }
 }
