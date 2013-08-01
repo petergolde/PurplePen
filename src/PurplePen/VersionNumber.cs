bdda00d451299065f1f5596f3e91bff6da364ec5
Peter Golde <peter@golde.org>
2013-08-01 10:59:27 -0700

    Update for release of 2.0.3

--- src/PurplePen/VersionNumber.cs
+++ src/PurplePen/VersionNumber.cs
@@ -44,7 +44,7 @@ namespace PurplePen
     // product code (you will be prompted for this -- say yes).
     static class VersionNumber
     {
-        public const string Current = "2.0.2.500";
+        public const string Current = "2.0.3.500";
 
         // The last component encodes the Alpha/Beta/RC/Stable notion.
         // 101 is Alpha 1, 220 is Beta 2, 500 is stable release
