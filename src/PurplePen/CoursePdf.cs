3c824aa0ffa9ba2d1abc748fe1bd9b4203355889
Peter Golde <peter@golde.org>
2017-09-01 12:16:30 -0700

    test commit

--- src/PurplePen/CoursePdf.cs
+++ src/PurplePen/CoursePdf.cs
@@ -1,5 +1,5 @@
 ﻿/* Copyright (c) 2006-2008, Peter Golde
- * All rights reserved.
+ * All rights reserved. 
  * 
  * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are 
