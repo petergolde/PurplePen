3902e4fdd123226e11c351dcec023415622cfc4a
Peter Golde <peter@golde.org>
2022-09-29 12:37:53 -0700

    TitleDetailButton high dpi

--- src/PurplePen/TitleDetailButton.Designer.cs
+++ src/PurplePen/TitleDetailButton.Designer.cs
@@ -30,13 +30,17 @@
             this.pictureBox = new System.Windows.Forms.PictureBox();
             this.titleLabel = new System.Windows.Forms.Label();
             this.detailLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox
             // 
             this.pictureBox.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox.Location = new System.Drawing.Point(5, 5);
+            this.pictureBox.Location = new System.Drawing.Point(3, 3);
             this.pictureBox.Name = "pictureBox";
             this.pictureBox.Size = new System.Drawing.Size(64, 64);
             this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -50,11 +54,12 @@
             // 
             this.titleLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.titleLabel, 2);
             this.titleLabel.Font = new System.Drawing.Font("Segoe UI", 11F);
             this.titleLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.titleLabel.Location = new System.Drawing.Point(72, 5);
+            this.titleLabel.Location = new System.Drawing.Point(3, 0);
             this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(165, 25);
+            this.titleLabel.Size = new System.Drawing.Size(148, 25);
             this.titleLabel.TabIndex = 1;
             this.titleLabel.Text = "label1";
             this.titleLabel.Click += new System.EventHandler(this.DoClick);
@@ -67,22 +72,49 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.detailLabel.Font = new System.Drawing.Font("Segoe UI", 8F);
-            this.detailLabel.Location = new System.Drawing.Point(81, 30);
+            this.detailLabel.Location = new System.Drawing.Point(23, 25);
             this.detailLabel.Name = "detailLabel";
-            this.detailLabel.Size = new System.Drawing.Size(157, 49);
+            this.detailLabel.Size = new System.Drawing.Size(128, 49);
             this.detailLabel.TabIndex = 2;
             this.detailLabel.Text = "label2";
             this.detailLabel.Click += new System.EventHandler(this.DoClick);
             this.detailLabel.MouseEnter += new System.EventHandler(this.MouseEntered);
             this.detailLabel.MouseLeave += new System.EventHandler(this.MouseLeft);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.titleLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.detailLabel, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(86, 7);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(154, 74);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.pictureBox);
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(243, 84);
+            this.panel1.TabIndex = 4;
+            // 
             // TitleDetailButton
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.Controls.Add(this.detailLabel);
-            this.Controls.Add(this.titleLabel);
-            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.Name = "TitleDetailButton";
             this.Size = new System.Drawing.Size(243, 84);
@@ -90,6 +122,8 @@
             this.MouseEnter += new System.EventHandler(this.MouseEntered);
             this.MouseLeave += new System.EventHandler(this.MouseLeft);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -99,5 +133,7 @@
         private System.Windows.Forms.PictureBox pictureBox;
         private System.Windows.Forms.Label titleLabel;
         private System.Windows.Forms.Label detailLabel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
