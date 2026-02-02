300341f0fbf01245ea0a5cddf54df7fa5d2a758e
petergolde <peter@golde.org>
2026-02-02 14:03:35 -0800

    Change MapModel to SDK-style project Change Test_MapModel to SDK-style project. Change Map_GDIPlus, Map_PDF, Map_PDF_Tests to SDK-style project. Change Map_WPF, Test_MapWPF, ProjectionDump to Sdk Style

--- src/MapModel/Test_MapWPF/RenderingTests.cs
+++ src/MapModel/Test_MapWPF/RenderingTests.cs
@@ -25,25 +25,13 @@ namespace TestWpfMap
     [TestClass]
     public class RenderingTests
     {
-        private TestContext testContextInstance;
-
         private const int MAX_PIXEL_DIFFERENCE = 30;
 
         /// <summary>
         ///Gets or sets the test context which provides
         ///information about and functionality for the current test run.
         ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
+        public TestContext TestContext { get; set; }
 
         #region Additional test attributes
         //
