8344230ca394bdc5c6f13b06f553da26a579ceb7
Jones Jankovic <outerrim1977@gmail.com>
2026-09-05 21:40:54 +0930

    Add license file and clean up leftover gitignore and csproj entries

--- src/PurplePen/LicenseForm.cs
+++ src/PurplePen/LicenseForm.cs
@@ -49,12 +49,12 @@ namespace PurplePen
 
             licenseTextBox.Rtf =
 @"{\rtf1\ansi\ansicpg1252\deff0\deflang1033{\fonttbl{\f0\froman\fprq2\fcharset0 Times New Roman;}{\f1\froman\fprq2\fcharset2 Symbol;}{\f2\fnil\fcharset0 Microsoft Sans Serif;}}
-\viewkind4\uc1\pard\sb100\sa100\f0\fs24 Copyright \'a9 2007-2012, Peter Golde\line All rights reserved.\par
+\viewkind4\uc1\pard\sb100\sa100\f0\fs24 Copyright \'a9 2006-2026 Peter Golde\par
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:\par
 \pard\fi-360\li720\sb100\sa100\tx720\f1\fs20\'b7\tab\f0\fs24 Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.\par
 \pard\fi-360\li720\sb100\sa100\f1\fs20\'b7\tab\f0\fs24 Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.\par
 \f1\fs20\'b7\tab\f0\fs24 Neither the name of Peter Golde, nor ""Purple Pen"", nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.\par
-\pard\sb100\sa100 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ""AS IS"" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\par
+\pard\sb100\sa100 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ""AS IS"" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\par
 \pard\f2\fs17\par
 }";
         }
